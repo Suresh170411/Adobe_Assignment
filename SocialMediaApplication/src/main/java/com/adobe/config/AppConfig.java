9ba78e7f8a2f9ecf32ba65b7a4e01aaa382b70e3
suresh170411 <suresh.20fk@gmail.com>
2023-04-09 15:35:32 +0530

    Cors configuration integrated to the application

--- SocialMediaApplication/src/main/java/com/adobe/config/AppConfig.java
+++ SocialMediaApplication/src/main/java/com/adobe/config/AppConfig.java
@@ -9,6 +9,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class AppConfig {
@@ -20,6 +26,24 @@ public class AppConfig {
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and()
 		.csrf().disable()
+		.cors().configurationSource(new CorsConfigurationSource() {
+			
+			@Override
+			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+				
+				CorsConfiguration cfg = new CorsConfiguration();
+				
+				cfg.setAllowedOrigins(Collections.singletonList("*"));
+				
+				cfg.setAllowedMethods(Collections.singletonList("*"));
+				cfg.setAllowCredentials(true);
+				cfg.setAllowedHeaders(Collections.singletonList("*"));
+				cfg.setExposedHeaders(Arrays.asList("Authorization"));
+				cfg.setMaxAge(3600L);
+				return cfg;
+			}
+		})
+		.and()
 		.authorizeHttpRequests()
 		.requestMatchers(HttpMethod.POST, "/users").permitAll()
 		.requestMatchers("/swagger-ui*/**","/v3/api-docs/**").permitAll()
